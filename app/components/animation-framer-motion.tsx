1005187cca2124b8c45d2b4c29284859244585cd
banexxl <damjanovic.branislav@gmail.com>
2025-05-08 08:49:07 +0200

    refactor: Rename Animate component to Transition and update animation properties for consistency

--- app/components/animation-framer-motion.tsx
+++ app/components/animation-framer-motion.tsx
@@ -1,19 +1,17 @@
 "use client";
 
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
-export default function Transition({
-     children,
-}: {
-     children: React.ReactNode;
-}) {
+export default function Animate({ children }: { children: React.ReactNode }) {
+
      return (
           <motion.div
-               initial={{ y: 20, opacity: 0 }}
-               animate={{ y: 0, opacity: 1 }}
-               transition={{ duration: 1.5 }}
+               initial={{ opacity: 0, scale: 0.98 }}
+               animate={{ opacity: 1, scale: 1 }}
+               transition={{ duration: 1.2, ease: [0.25, 0.1, 0.25, 1] }}
           >
                {children}
-          </motion.div>
+          </motion.div >
      );
 }
