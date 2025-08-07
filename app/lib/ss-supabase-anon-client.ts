1c1771ab8115a15d2dd54305f5f6f8a30bfadd5d
banexxl <damjanovic.branislav@gmail.com>
2025-08-07 19:53:37 +0200

    refactor: Remove accessToken parameter from useServerSideSupabaseAnonClient function

--- app/lib/ss-supabase-anon-client.ts
+++ app/lib/ss-supabase-anon-client.ts
@@ -1,7 +1,7 @@
 import { createServerClient } from '@supabase/ssr';
 import { cookies } from 'next/headers';
 
-export const useServerSideSupabaseAnonClient = async (accessToken?: string) => {
+export const useServerSideSupabaseAnonClient = async () => {
      // Use the server-side Supabase client
      const cookieStore = await cookies();
      const supabase = createServerClient(
