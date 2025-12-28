c823a2ec808a4e3046228662d9f1e0f210919eb2
banexxl <damjanovic.branislav@gmail.com>
2025-12-28 19:44:17 +0100

    feat: remove console log for Polar access token to clean up code

--- app/lib/polar.ts
+++ app/lib/polar.ts
@@ -1,7 +1,5 @@
 import { Polar } from "@polar-sh/sdk";
 
-console.log('Using Polar access token:', process.env.NODE_ENV === "development" ? process.env.POLAR_ACCESS_TOKEN_SANDBOX : process.env.POLAR_ACCESS_TOKEN);
-
 export const polar = new Polar({
      accessToken: process.env.NODE_ENV === "development" ? process.env.POLAR_ACCESS_TOKEN_SANDBOX! : process.env.POLAR_ACCESS_TOKEN!,
      // Polar supports sandbox vs production; choose based on env.
