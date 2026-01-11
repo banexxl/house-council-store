976c007c1710c657fb4708824826a3acf72deba3
banexxl <damjanovic.branislav@gmail.com>
2026-01-11 11:59:04 +0100

    refactor: simplify getSessionUser by removing error logging and handling

--- app/lib/get-session.ts
+++ app/lib/get-session.ts
@@ -2,35 +2,13 @@
 
 import { User } from "@supabase/supabase-js";
 import { useServerSideSupabaseAnonClient } from "./ss-supabase-anon-client";
-import { logServerAction } from "./server-logging";
 
 export const getSessionUser = async (): Promise<User | null> => {
 
      const supabase = await useServerSideSupabaseAnonClient()
 
      // Retrieve the authenticated user
-     const { data, error } = await supabase.auth.getUser()
+     const { data } = await supabase.auth.getUser()
 
-     if (error) {
-          await logServerAction({
-               user_id: null,
-               action: 'Get Session User - Error during supabase getUser.',
-               payload: {},
-               status: 'fail',
-               error: error.message,
-               duration_ms: 0,
-               type: 'auth'
-          })
-          return null
-     }
-     await logServerAction({
-          user_id: null,
-          action: 'Get Session User - Success.',
-          payload: {},
-          status: 'success',
-          error: '',
-          duration_ms: 0,
-          type: 'auth'
-     })
      return data.user
 }
