bf261b4a5f29751f31d0510cbf526608346501d5
banexxl <damjanovic.branislav@gmail.com>
2026-05-03 18:59:43 +0200

    feat: add Google Analytics tracking script to RootLayout for improved user analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18137335805"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'AW-18137335805');
+            `,
+          }}
+        />
       </head>
       <body className={inter.className}>
         <Providers>{children}</Providers>
